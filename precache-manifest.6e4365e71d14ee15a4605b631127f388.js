895f733b7e54077af68778f05158ddfdf1a2f139
cishocksr <cishockleysr@gmail.com>
2019-11-29 08:26:07 -0500

    Updates

---
+++ precache-manifest.6e4365e71d14ee15a4605b631127f388.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00f573635bbbf9327f6914053c0242d",
+    "url": "https//cishocksr.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6a2c340a79dadca8550c",
+    "url": "https//cishocksr.github.io/monsters-rolodex/static/css/main.d30e4220.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "https//cishocksr.github.io/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "6a2c340a79dadca8550c",
+    "url": "https//cishocksr.github.io/monsters-rolodex/static/js/main.3d81d4ff.chunk.js"
+  },
+  {
+    "revision": "de440e1a538f93194012",
+    "url": "https//cishocksr.github.io/monsters-rolodex/static/js/runtime-main.9eb4c34d.js"
+  }
+]);
